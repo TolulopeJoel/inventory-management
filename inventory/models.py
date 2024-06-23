6a0ed37642702db8c6f247e1f295b7d2e84f9435
Tolu Joel <toluisjoel@gmail.com>
2024-06-23 22:52:30 +0100

    feat: Add new field `quantity` to InventoryItem model

--- inventory/models.py
+++ inventory/models.py
@@ -13,6 +13,7 @@ class InventoryItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=0)
     suppliers = models.ManyToManyField(Supplier, related_name='items')
 
     date_added = models.DateTimeField(auto_now_add=True)
