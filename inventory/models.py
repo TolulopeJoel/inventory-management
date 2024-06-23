4adfadcbc3d320346b5880ee66360e8d9ac5b34c
Tolu Joel <toluisjoel@gmail.com>
2024-06-23 19:21:02 +0100

    feat: Add Supplier and InventoryItem models.

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100)
+    contact_info = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class InventoryItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    suppliers = models.ManyToManyField(Supplier, related_name='items')
+
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
