a7a472b1e755664f320e9fa08b8ca5d3ab7bda4f
Tolu Joel <toluisjoel@gmail.com>
2024-06-24 06:06:47 +0100

    feat: Add employee permissions.

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
+from accounts.permissions import IsEmployee
 
 from .models import InventoryItem, Supplier
 from .serializers import InventoryItemSerializer, SupplierSerializer
@@ -7,8 +10,15 @@ from .serializers import InventoryItemSerializer, SupplierSerializer
 class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
+    permission_classes = [IsAuthenticated, IsEmployee]
 
 
 class InventoryItemViewSet(viewsets.ModelViewSet):
     queryset = InventoryItem.objects.all()
     serializer_class = InventoryItemSerializer
+    permission_classes = [IsAuthenticated, IsEmployee]
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        return [IsAuthenticated(), IsEmployee()]
